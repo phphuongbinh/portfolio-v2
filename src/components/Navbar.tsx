00759a13c95ee9565af7400754a1e423c1c780e1
ErauQsPhan <mrsplol102@gmail.com>
2024-03-16 22:02:58 +0700

    setup project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium">
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/articles">Articles</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        mont: ['var(--font-mont)'],
+      },
+      colors: {
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#B63E96',
+        primaryDark: '#58E6D9',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
