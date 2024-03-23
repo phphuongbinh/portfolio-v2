330d4c50f53f1b932ab14a9515d5997901966666
ErauQsPhan <mrsplol102@gmail.com>
2024-03-23 23:39:32 +0700

    update

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,6 +4,9 @@ const Details = ({ company, time, address, desc }: any) => (
   <li>
     <div>
       <h3>{company}</h3>
+      <span>
+        {time} | {address}
+      </span>
     </div>
   </li>
 );
@@ -12,7 +15,9 @@ const Education = () => {
   return (
     <div className="my-48">
       <h2 className="font-bold text-8xl mb-32 w-full text-center">Education</h2>
-      <div className="w-[75%] mx-auto relative"></div>
+      <div className="w-[75%] mx-auto relative">
+        <Details company="test"></Details>
+      </div>
     </div>
   );
 };
