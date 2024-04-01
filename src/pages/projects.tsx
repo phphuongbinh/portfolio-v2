f30dceaaa4ac4ad7ce8e54110da108721be1ab99
ErauQsPhan <mrsplol102@gmail.com>
2024-04-01 17:01:56 +0700

    update project page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -24,28 +24,30 @@ const FeaturedProject = ({
   github = "",
 }: FeaturedProjectProps) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl space-x-6 p-12">
-      <div className="absolute -z-0 top-0 -right-3 w-[102%] h-[103%] bg-dark rounded-3xl"></div>
-      <Link href={link} target="_blank" className="w-1/2 ">
-        <Image src={img} alt="" className="w-full h-auto rounded-lg"></Image>
-      </Link>
-      <div className="w-1/2 ">
-        <span className="text-xl text-primary font-medium ">{type}</span>
-        <Link href={link} target="_blank">
-          <h2 className="text-4xl font-bold hover:underline my-3">{title}</h2>
+    <article className="relative">
+      <div className="absolute -z-0 top-0 -right-4 w-[100%] h-[103%] bg-dark rounded-3xl"></div>
+      <div className="w-full relative z-10 flex items-center justify-between rounded-3xl border-2 border-solid border-dark bg-light shadow-2xl space-x-6 p-12">
+        <Link href={link} target="_blank" className="w-1/2 ">
+          <Image src={img} alt="" className="w-full h-auto rounded-lg"></Image>
         </Link>
-        <p className="font-medium text-dark">{summary}</p>
-        <div className="flex items-center gap-4 mt-3">
-          <Link href={github} target="_blank">
-            <GithubIcon />
+        <div className="w-1/2 ">
+          <span className="text-xl text-primary font-medium ">{type}</span>
+          <Link href={link} target="_blank">
+            <h2 className="text-4xl font-bold hover:underline my-3">{title}</h2>
           </Link>
-          <Link
-            href={link}
-            target="_blank"
-            className="bg-dark text-white px-6 py-3 rounded-lg font-semibold"
-          >
-            Visit Project
-          </Link>
+          <p className="font-medium text-dark">{summary}</p>
+          <div className="flex items-center gap-4 mt-3">
+            <Link href={github} target="_blank">
+              <GithubIcon />
+            </Link>
+            <Link
+              href={link}
+              target="_blank"
+              className="bg-dark text-white px-6 py-3 rounded-lg font-semibold"
+            >
+              Visit Project
+            </Link>
+          </div>
         </div>
       </div>
     </article>
@@ -69,7 +71,7 @@ const Project = ({
 }: ProjectProps) => {
   return (
     <article className=" relative">
-      <div className="absolute -z-0 top-0 -right-3 w-[102%] h-[103%] bg-dark rounded-3xl"></div>
+      <div className="absolute -z-0 top-0 -right-4 w-[102%] h-[102%] bg-dark rounded-3xl"></div>
       <div className="w-full flex flex-col items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-6 relative z-10">
         <Link href={link} target="_blank" className="w-full">
           <Image src={img} alt="" className="w-full h-auto rounded-lg"></Image>
